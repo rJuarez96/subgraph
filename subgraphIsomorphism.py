62318cec0dc2bc60564deaa12bff2db970b594c1
Roberto <joseroberto.juarez@gmail.com>
2016-05-14 10:14:54 -0500

    In

--- subgraphIsomorphism.py
+++ subgraphIsomorphism.py
@@ -79,7 +79,7 @@ def permute(m0,row,h,g): #Crea todas las permutaciones de la matriz padre M0 pos
                 m0[row][n]=0 #mark c as unused
         return False"""
 def create(formula):
-    return formula
+    
     mat = getfor(formula) 
     ah=mat[0]
     ag=mat[1]
@@ -103,7 +103,7 @@ def getfor(formula):
     
     
     clause1=hacMa(clauses[0][3:-2])
-    clause2=hacMa(clauses[1][2:-3])
+    clause2=hacMa(clauses[1][3:-2])
     retCla.append(clause1)
     retCla.append(clause2)
 
