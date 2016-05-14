2b15373e2fcd49a405897bd45ecca9da48768ea8
Roberto <joseroberto.juarez@gmail.com>
2016-05-14 18:56:32 -0500

    In

--- subgraphIsomorphism.py
+++ subgraphIsomorphism.py
@@ -1,52 +1,58 @@
-import networkx as nx
-import itertools
-
-G=nx.Graph()
 import time
 start_time=time.time()
+steps=0
 
-def countVertexDegree(matrix): #Crea una lista con los grados de cada vrtice en el grafo que recibe
+def countVertexDegree(matrix): #Crea una lista con los grados de cada vértice en el grafo que recibe
+    global steps
     degree=[]
     deg=0
+    
     for row in matrix:
         for bit in row:
             if bit==1:
                 deg+=1
+                steps+=1
         degree.append(deg)
         deg=0
     return degree
     
-def buildM(h,g): #Crea una matriz M0 comparando los grados de cada vrtice de los grafos H y G
+def buildM(h,g): #Crea una matriz M0 comparando los grados de cada vértice de los grafos H y G
     m0=[]
     for n in range(len(g)):
         m0.append([])
     for i in range(len(g)):
         for j in range(len(h)):
-            m0[i].append((h[j]>=g[i]).real) #1 si el grado del vrtice Vhj >= grado del vrtice Vgi
+            m0[i].append((h[j]>=g[i]).real) #1 si el grado del vértice Vhj >= grado del vértice Vgi
     return m0
 
 def prune(m,i,j,ag,ah):
-    #print(i,j,m)
-    #print("\n")
+    global steps
     for x in range(len(ag)):
         if(ag[i][x]):
             for y in range(len(ah)):
+                steps+=1
                 if(m[x][y]*ah[y][j]):
                     return True
     
-def permute(m0,row,h,g): #Crea todas las permutaciones de la matriz padre M0 posibles y prueba si son isomrficas
+def permute(m0,row,h,g): #Crea todas las permutaciones de la matriz padre M0 posibles y prueba si son isomórficas
+    #for r in m0:
+    #    print(r)
+    #print("\n")
+    global steps
     if(row==len(m0)):
         n=0
         for i in range(len(m0[0])):
             for j in range(len(m0)):
                 n+=m0[j][i]
+                steps+=1
             if(n>1):
                 return
             n=0
-        
         res=[[sum(a*b for a,b in zip(m0_row,h_col)) for h_col in zip(*h)] for m0_row in m0]
+        steps+=((2*len(h)-1)*(len(res)*len(res[0])))
         resTr=[[res[j][i] for j in range(len(res))] for i in range(len(res[0]))]
         arr=[[sum(a*b for a,b in zip(m0_row,resTr_col)) for resTr_col in zip(*resTr)] for m0_row in m0]
+        steps+=((2*len(resTr)-1)*(len(arr)*len(arr[0])))
         if(g==arr): #<= para subgrafos inducidos
             for ro in m0:
                 print(ro)
@@ -54,8 +60,8 @@ def permute(m0,row,h,g): #Crea todas las permutaciones de la matriz padre M0 pos
             return True
     else:
         arr1=m0[row][:] #Guarda copia de m0[row]
-        arr2=[i for i, x in enumerate(arr1) if x] #Crea una lista con los ndices donde hay 1 en arr1
-        for n in range(len(arr2)): #Cambia todos los 1 a 0 excepto el n-simo
+        arr2=[i for i, x in enumerate(arr1) if x] #Crea una lista con los índices donde hay 1 en arr1
+        for n in range(len(arr2)): #Cambia todos los 1 a 0 excepto el n-ésimo
             for i in arr2:
                 if i!=arr2[n]:
                     m0[row][i]=0
@@ -67,19 +73,9 @@ def permute(m0,row,h,g): #Crea todas las permutaciones de la matriz padre M0 pos
                     m0[row]=arr1[:]
             else:
                 m0[row]=arr1[:]
-        return False
-
-        """for n in range(len(m0[0])):
-            m0[row][n]=0
-        for n in range(len(m0[0])):
-            m0[row][n]=1 #mark c as used
-            if permute(m0,row+1,h,g):
-                return True    
-            else:
-                m0[row][n]=0 #mark c as unused
-        return False"""
+        return False        
 def create(formula):
-    
+    global steps
     mat = getfor(formula) 
     ah=mat[0]
     ag=mat[1]
@@ -88,7 +84,7 @@ def create(formula):
     m0=buildM(degh,degg)
     
     if (permute(m0,0,ah,ag)):
-        return("YES!")
+        return("YES!   "+str(steps))
     else:
         return("No")
     
