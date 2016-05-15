05e46a4658fb20e429edee256d1d87e810514826
Roberto <joseroberto.juarez@gmail.com>
2016-05-14 20:33:08 -0500

    In

--- app.py
+++ app.py
@@ -9,16 +9,19 @@ def index():
     if request.method == 'POST':
         clauses = request.form.get('clauses',False,type=str)
         clauses2 = request.form.get('clauses2',False,type=str)
-        #clausesCon=str(clauses)+"/"+str(clauses2)
-        #clausesCon2=str(clauses)+"------------------------"+str(clauses2)
+        clausesCon=str(clauses)+"/"+str(clauses2)
+        clausesCon2=str(clauses)+"------------------------"+str(clauses2)
         test=request.form.get('clausesN',False,type=str)
         test2=request.form.get('clausesN2',False,type=str)
 
-        if(not clauses and not test):
-            return render_template('index.html',clausula = clausesCon2,maxclique="Ingrese un grafo")
-        #maxclique = subgraphIsomorphism.create(str(clausesCon))
-        maxclique=str(test)
-        return render_template('index.html',clausula = str(test2),maxclique = maxclique )
+
+        if(not clauses):
+            if(not test):
+                return render_template('index.html',clausula = clausesCon2,maxclique="Ingrese un grafo")
+            maxclique = subgraphIsomorphism.create(str(clausesCon))        
+        maxclique = subgraphIsomorphism.create(str(clausesCon))
+        
+        return render_template('index.html',clausula = str(clausesCon2),maxclique = maxclique )
     else:
         return render_template('index.html',maxclique="Ingrese un grafo")
 	
